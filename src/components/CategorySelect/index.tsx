089fea7dd11ba8f8971da93dacfd3546224de505
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-08-22 12:58:24 -0300

    chore(ListDivider): adjust margin

--- src/components/CategorySelect/index.tsx
+++ src/components/CategorySelect/index.tsx
@@ -10,13 +10,30 @@ import { categories } from '../../utils/categories';
 type Props = {
   categorySelected: string;
   setCategory: (categoryId: string) => void;
+  hasCheckBox?: boolean;
 };
 
-export function CategorySelect({ categorySelected, setCategory }: Props) {
+export function CategorySelect({
+  categorySelected,
+  setCategory,
+  hasCheckBox = false,
+}: Props) {
   return (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{ paddingRight: 40 }} style={styles.container}>
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{ paddingRight: 40 }}
+      style={styles.container}
+    >
       {categories.map((category) => (
-        <Category key={category.id} title={category.title} icon={category.icon} checked={category.id === categorySelected} onPress={() => setCategory(category.id)} />
+        <Category
+          key={category.id}
+          title={category.title}
+          icon={category.icon}
+          checked={category.id === categorySelected}
+          onPress={() => setCategory(category.id)}
+          hasCheckBox={hasCheckBox}
+        />
       ))}
     </ScrollView>
   );
